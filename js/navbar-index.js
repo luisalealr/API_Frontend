1f6f6ed1dd0bce342a0f8578947df922b4bb16de
Luisa 3 <106846834+luisalealr@users.noreply.github.com>
2024-05-12 21:38:56 -0500

    footer

--- js/navbar-index.js
+++ js/navbar-index.js
@@ -1,9 +1,19 @@
 const nav =document.querySelector('.navegacion')
+const foot =document.querySelector('.footer')
 
 fetch('../views/navbar-index.html')
 .then(res=>res.text())
 .then(data=>{
     nav.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(data, 'text/html')
+    eval(doc.querySelector('script').textContent)
+})
+
+fetch('../views/footer.html')
+.then(res=>res.text())
+.then(data=>{
+    foot.innerHTML=data
     const parser = new DOMParser()
     const doc = parser.parseFromString(data, 'text/html')
     eval(doc.querySelector('script').textContent)
